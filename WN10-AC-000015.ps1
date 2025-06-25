34eeee53a6f72a9aa0c34b8242509c73dc6b25b6
Jason Nguyen <jpnwriter@gmail.com>
2025-06-25 22:10:08 +0900

    Update WN10-AC-000015.ps1

--- WN10-AC-000015.ps1
+++ WN10-AC-000015.ps1
@@ -12,6 +12,8 @@
     Date Created    : 2025-06-24
     Last Modified   : 2025-06-24
     Version         : 2.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
     STIG-ID         : WN10-AC-000015
 
 .TESTED ON
